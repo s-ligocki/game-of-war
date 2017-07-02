eda4275f35875f3b401ba682d1891444a50b9bd3
JakubWienzek <contact@jakubwienzek.eu>
2017-07-02 15:37:26 +0200

    Cleaning of the codebase. Just needed files - raw angular project. No Karma, no Bower, no external components.

--- game-of-war-front/app/app.js
+++ game-of-war-front/app/app.js
@@ -1,15 +1,17 @@
-'use strict';
+(function () {
 
+'use strict';
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-])
-    .controller('Hello', function($scope, $http) {
-        $http.get('http://localhost:8080/get-new-board').
-        then(function(response) {
-            $scope.board = response.data;
-        });
+var myapp = angular.module('myApp', [])
+
+myapp.controller('Hello', function($scope, $http) {
+    $http.get('http://localhost:8080/get-new-board').
+    then(function(response) {
+        $scope.board = response.data;
     });
+});
+
+myapp.controller('test', function($scope) {
+    $scope.variable = "a string for testing";
+});
+})();
